5f35c998ed10a9cf0f1c998fcbe9ff21e83645b8
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 05:54:55 -0400

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    num1=num1+num2;
+    num2=num1-num2;
+    num1=num1-num2;
+    printf("After swapping: %d %d",num1,num2);
+}
