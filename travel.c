a5ba435bcc7d629be461b69fc99bac7488436a6f
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 05:53:40 -0400

    Add files via upload

---
+++ travel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, small;
+    printf("Enter any two number: ");
+    scanf("%d%d", &a, &b);
+    if(a<b)
+        small=a;
+    else
+        small=b;
+    printf("\nSmallest of the two number is: %d", small);
+    getch();
+    return 0;
+}
